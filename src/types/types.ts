48ddc6a79759538f502c7225e4e072a74e955dda
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-11 09:38:56 -0300

    types

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,17 @@ import React from "react";
 export interface Children {
   children: React.ReactNode
 }
+
+export interface WeatherContext {
+  coords: {
+    longitude: number | null
+    latitude: number
+  }
+}
+
+export interface Location {
+  name: string
+  sys: {
+    country: string
+  }
+}
