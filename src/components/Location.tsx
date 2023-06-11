c0e0577e3179982540931d87bdc4e07d3127aac0
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-11 09:39:04 -0300

    components

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,11 +1,25 @@
-import { MapPin } from 'lucide-react'
-import React from 'react'
+import React, { useContext } from 'react'
 
-const Location = () => {
+import axios from 'axios'
+
+import { WeatherContxt } from '@/contexts/WeatherContext'
+import { MapPin } from 'lucide-react'
+import { Location } from '@/types/types'
+
+const apiKey = process.env.NEXT_PUBLIC_API_KEY
+
+const Location = async () => {
+
+  const { coords } = useContext(WeatherContxt)
+
+  const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${apiKey}`)
+
+  const location: Location = await response.data
+
   return (
     <span className='absolute top-6 right-0 mr-8 text-vodka text-xl flex items-center gap-2'>
       <MapPin />
-      Rio do Sul, SC
+      {location.name}, {location.sys.country}
     </span>
   )
 }
--- src/components/TemperatureNow.tsx
+++ src/components/TemperatureNow.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useContext } from 'react'
 
 import Location from './Location'
 import CloudyIcon from './icons/CloudyIcon'
@@ -6,12 +7,13 @@ import Statistics from './Statistics'
 import CurrentWeather from './CurrentWeather'
 
 const TemperatureNow = () => {
+
   return (
     <div className='relative text-white h-[480px] w-[480px] rounded-lg bg-clouds bg-center bg-cover flex flex-col items-center justify-end'>
      <div className='absolute -top-16 -left-12'>
       <CloudyIcon />
      </div>
-
+  
      <Location />
 
      <div className='w-full h-2/3 flex flex-col items-center justify-between pb-4'>
