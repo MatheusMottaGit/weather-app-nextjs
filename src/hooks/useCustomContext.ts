5087e5d757f275d59deb0377f2d9744b10641e2e
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-11 10:52:36 -0300

    deleted

--- src/hooks/useCustomContext.ts
+++
@@ -1,7 +0,0 @@
-import { ServerContext, ServerContextJSONValue, useContext } from "react"
-
-export const useCustomContext = (context: ServerContext<ServerContextJSONValue>) => {
-  const contxt = useContext(context)
-
-  return contxt
-}
